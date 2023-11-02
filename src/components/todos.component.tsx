c8f84d82e434e3bd80c51c7f0134a65a2e2ce67a
jorgeguedess <jorge_guedes00@hotmail.com>
2023-11-02 19:29:05 -0300

    feat: test should add task on add click

--- src/components/todos.component.tsx
+++ src/components/todos.component.tsx
@@ -1,5 +1,32 @@
 import { AiOutlinePlus } from "react-icons/ai";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { v4 as uuid } from "uuid";
+
+import { Todo } from "../interfaces/todos";
+
 const Todos = (): JSX.Element => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    resetField,
+  } = useForm<{ title: string }>();
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAddClick = (data: { title: string }) => {
+    setTodos((prev) => [
+      ...prev,
+      {
+        id: uuid(),
+        title: data.title,
+        isCompleted: false,
+      },
+    ]);
+    resetField("title");
+  };
+
   return (
     <div className="w-screen h-screen bg-gray-950 flex items-center justify-center">
       <div className="flex flex-col items-center justify-center p-6 rounded-md bg-gray-800 gap-6">
@@ -9,11 +36,22 @@ const Todos = (): JSX.Element => {
             className="rounded-md p-3 text-gray-50 
           bg-gray-600 flex items-center justify-center"
             placeholder="Digite o nome da tarefa"
+            {...register("title")}
           />
-          <button aria-label="Adicionar tarefa" className="text-gray-50">
+          <button
+            aria-label="Adicionar tarefa"
+            className="text-gray-50"
+            onClick={() => handleSubmit(handleAddClick)()}
+          >
             <AiOutlinePlus size={24} />
           </button>
         </div>
+
+        {todos.map((todo) => (
+          <p className="text-gray-50" key={todo.id}>
+            {todo.title}
+          </p>
+        ))}
       </div>
     </div>
   );
--- src/components/todos.test.tsx
+++ src/components/todos.test.tsx
@@ -1,4 +1,5 @@
 import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import Todos from "./todos.component";
 
@@ -21,5 +22,25 @@ describe("Todos", () => {
     render(<Todos />);
 
     expect(screen.queryByLabelText("Adicionar tarefa")).toBeInTheDocument();
+  });
+
+  it("should add task on add click", async () => {
+    render(<Todos />);
+
+    const input = screen.getByPlaceholderText("Digite o nome da tarefa");
+
+    const taskTitle = "Nova tarefa";
+
+    await userEvent.type(input, taskTitle);
+
+    screen.getByDisplayValue(taskTitle);
+
+    const addButton = screen.getByLabelText("Adicionar tarefa");
+
+    await userEvent.click(addButton);
+
+    screen.getByPlaceholderText("Digite o nome da tarefa");
+
+    expect(screen.queryAllByText(taskTitle)).toHaveLength(1);
   });
 });
